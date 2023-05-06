03fa3e7fd37f0a9a6f463048a708a4b4a07eb287
Samandar Mansurov <130559675+samandar-dev-fronted@users.noreply.github.com>
2023-05-06 21:30:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const formOpenBtn = document.querySelector('#form_open'),
+   home = document.querySelector('.home'),
+   formContainer = document.querySelector('.form_container'),
+   formCloseBtn = document.querySelector('.form_close'),
+   signupBtn = document.querySelector('#signup'),
+   loginBtn = document.querySelector('#login'),
+   pwShowHide = document.querySelector('.pw_hide');
+
+  
+
+
+formOpenBtn.addEventListener("click" , () => home.classList.add("show"))
+formCloseBtn.addEventListener("click" , () => home.classList.remove("show"));
+
+
+signupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.add("active")
+} )
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.remove("active")
+} );
+
+
+
+
